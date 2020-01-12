0acc4df976b1c4b46c234de77998877c523fc002
Mohammed Abadi <mohammad.abadi@gotocme.com>
2020-01-12 23:31:50 +0200

    Created the skeleton of the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from './core/auth/auth-guard.service';
+import { LoginComponent } from './core/login/login.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes =
+  [
+    {
+      path: '', loadChildren: './modules/dashboard/dashboard.module#DashboardModule'
+      , canActivate: [AuthGuardService], runGuardsAndResolvers: 'always'
+    },
+    { path: 'login', component: LoginComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './core/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    SharedModule, // Import Shared Module (This to imported here and in feature modules)
+    CoreModule, // Import Core Module (Only imported in app.module and only have providers:[])
   ],
   providers: [],
   bootstrap: [AppComponent]
